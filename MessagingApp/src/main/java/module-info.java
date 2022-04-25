bc1c81ca99c48df833e9e0860457767510e1a712
Ancross333 <101778926+Ancross333@users.noreply.github.com>
2022-04-24 17:35:39 -0700

    Delete MessagingApp directory

--- MessagingApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.messagingapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.messagingapp to javafx.fxml;
-    exports com.example.messagingapp;
-}
